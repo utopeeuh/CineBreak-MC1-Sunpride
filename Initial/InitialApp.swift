50fbac9cfa65e125fd27e4e4a8308eeb70bba34a
ramdanks <ramadhan.kalih@ui.ac.id>
2022-03-30 11:31:49 +0700

    initial app

---
+++ Initial/InitialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InitialApp.swift
+//  Initial
+//
+//  Created by Ramadhan Kalih Sewu on 28/02/22.
+//
+
+import SwiftUI
+
+@main
+struct InitialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
