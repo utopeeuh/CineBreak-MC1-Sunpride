d40d275e68fa039a661cbea813945e403c8a2102
ramdanks <ramadhan.kalih@ui.ac.id>
2022-04-07 08:36:51 +0700

    app logo, tab bar, navbar

--- MC1 Sunpride/ViewController.swift
+++ MC1 Sunpride/ViewController.swift
@@ -1,67 +1,20 @@
 //
 //  ViewController.swift
-//  MC1 Splash screen
+//  MC1 Sunpride
 //
-//  Created by Nikita Felicia on 07/04/22.
+//  Created by Tb. Daffa Amadeo Zhafrana on 04/04/22.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    /** this is optional, status bar style can be set in project settings */
+    override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
     
-//<<<<<<< HEAD
-//    @IBOutlet weak var timerLabel: UITextView!
-//    @IBOutlet weak var startStopButton: UIButton!
-//
-//    let userDefaults = UserDefaults.standard
-//
-//    var sessionTimer: SessionTimer?
-//    var sessions = getSessions()
-//
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
-        AppNotification.requestAuthorization()
-//        sessionTimer = SessionTimer(timerLabel: timerLabel, startStopButton: startStopButton)
     }
-//
-//    @IBAction func startStopAction(_ sender: Any) {
-//        sessionTimer!.startStopAction()
-//    }
-//
-//    func updateSessions(){
-//        if let data = try? PropertyListEncoder().encode(sessions) {
-//                UserDefaults.standard.set(data, forKey: SESSIONS_KEY)
-//        }
-//    }
-//=======
+
 }
-
-class HumanInterface
-{
-    static let blacklistName: [String] = ["iphone", "ipad"]
-    
-    static public func pickNameFromDevice() -> String?
-    {
-        let deviceName: String = UIDevice.current.name
-        let nameCandidates: [String] = deviceName.components(separatedBy: " ")
-        for name in nameCandidates
-            { if (!isBlacklisted(name) && !isBeginWithNumber(name)) { return name } }
-        return nil
-    }
-    
-    static private func isBlacklisted(_ string: String) -> Bool
-    {
-        for b in blacklistName
-            { if (string.localizedCaseInsensitiveContains(b)) { return true } }
-        return false
-    }
-    
-    static private func isBeginWithNumber(_ string: String) -> Bool
-    {
-        if string.isEmpty { return false }
-        return string[string.startIndex].isNumber
-    }
-}
-
