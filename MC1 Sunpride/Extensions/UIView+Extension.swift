4514ae6b80b770efa9a71599d542714445a68fef
nikitaafe <nikitafelicia88@gmail.com>
2022-04-07 18:38:48 +0700

    onboarding page

---
+++ MC1 Sunpride/Extensions/UIView+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+Extension.swift
+//  MC1 Sunpride
+//
+//  Created by Nikita Felicia on 07/04/22.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get { return cornerRadius }
+        set {
+            self.layer.cornerRadius = newValue
+        }
+    }
+}
