5df90ce45d7806437dbf97fa56e9db92509bdc94
Uto <daffamadeo@gmail.com>
2022-04-11 20:50:34 +0700

    stats + icons & prep-stretching modals

--- MC1 Sunpride/HomeViewController.swift
+++ MC1 Sunpride/HomeViewController.swift
@@ -5,9 +5,23 @@ class HomeViewController: UIViewController
 {
     /** this is optional, status bar style can be set in project settings */
     override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
+
+    @IBOutlet weak var prepBtn: UIButton!
+    @IBOutlet weak var stretchBtn: UIButton!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        prepBtn.semanticContentAttribute = .forceRightToLeft
+        stretchBtn.semanticContentAttribute = .forceRightToLeft
+    }
+    
+    @IBAction func openSetupModal(_ sender: Any) {
+        performSegue(withIdentifier: "setupModal", sender: self)
+    }
+    
+    @IBAction func openStretchingModal(_ sender: Any) {
+        performSegue(withIdentifier: "stretchingModal", sender: self)
     }
 }
--- MC1 Sunpride/PerformanceController.swift
+++ MC1 Sunpride/PerformanceController.swift
@@ -2,20 +2,46 @@ import UIKit
 
 class PerformanceController: UIViewController
 {
-    var breaksTakenPerHour : Double = 0
-    var avgWatchTime : Double = 0
+    var breaksTakenPerHour : String = "0"
+    var avgWatchTime : String = "0"
     
-    let totalWatchtime = UserPerformance.shared.totalWatchtime
+    let totalWatchtime = UserPerformance.shared.totalWatchtime/60
     let breaksTaken = UserPerformance.shared.breaksTaken
     let sessionsDone = UserPerformance.shared.sessionsDone
     let timesOvertime = UserPerformance.shared.timesOvertime
     
+    @IBOutlet weak var watchText: UILabel!
+    @IBOutlet weak var breaksText: UILabel!
+    @IBOutlet weak var avgWatchText: UILabel!
+    @IBOutlet weak var overtimeText: UILabel!
+    
+    @IBOutlet weak var watchLbl: UILabel!
+    
     override func viewDidLoad() {
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadData()
+    }
+    
+    private func loadData(){
         calculateStats()
+        
+        watchText.text = "Total Watch Time: \(roundDouble(x: totalWatchtime)) minutes"
+        breaksText.text = "Breaks: \(breaksTakenPerHour) per hour"
+        overtimeText.text = "Times Overtime: \(roundDouble(x: timesOvertime)) time(s)"
+        avgWatchText.text = "Average session time: \(avgWatchTime) minutes"
     }
     
     private func calculateStats(){
-        avgWatchTime = totalWatchtime / Double(sessionsDone)
-        breaksTakenPerHour = Double(breaksTaken) / (totalWatchtime/3600)
+        if totalWatchtime != 0{
+            avgWatchTime = roundDouble(x:(totalWatchtime / Double(sessionsDone)))
+            breaksTakenPerHour = roundDouble(x:(Double(breaksTaken) / (totalWatchtime/3600)))
+        }
+    }
+        
+    
+    private func roundDouble(x: Double) -> String{
+        return x == 0 ? "0" : String(format: "%.2f", x)
     }
 }
